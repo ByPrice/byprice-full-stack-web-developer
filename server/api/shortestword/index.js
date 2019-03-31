a8891549bcc5a79764c4a029574cf7ccaf4a6274
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2019-03-31 14:21:32 -0600

    Crated shortestword api and added mocha

--- server/api/shortestword/index.js
+++ server/api/shortestword/index.js
@@ -7,7 +7,30 @@
 const shortestword = async (ctx, next) => {
   const { list } = ctx.request.body;
 
-  console.log('list', list);
+  if (list && Array.isArray(list) === false) {
+    ctx.response.status = 400;
+    ctx.response.body = {
+      message: 'List should be an Array',
+    };
+  } else if (list && Array.isArray(list) && list.length >= 2) {
+    ctx.response.status = 400;
+    ctx.response.body = {
+      message: 'List min length allowed 2',
+    };
+  } else if (list && Array.isArray(list) && list.length <= 8) {
+    ctx.response.status = 400;
+    ctx.response.body = {
+      message: 'List max length allowed 8',
+    };
+  } else {
+    const lengthWords = list.map((item) => item.length);
+    const minLenght = Math.min(...lengthWords);
+
+    ctx.response.status = 200;
+    ctx.response.body = {
+      shortestLength: minLenght,
+    };
+  }
 
   await next();
 };
