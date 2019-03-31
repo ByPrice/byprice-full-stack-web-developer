91626822a4464e73c8c3f386b93ec999b1790215
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2019-03-31 12:34:01 -0600

    Resolved eslint issues in shortestword test

--- tests/shortestword.js
+++ tests/shortestword.js
@@ -1,39 +1,56 @@
-
-describe('Shortest world', function () {
-  it('Should return `4` as a shortest world length in the list', async function () {
+describe('Shortest world', () => {
+  it('Should return `4` as a shortest world length in the list', async () => {
     await this.agent
       .post('/api/shortestword')
       .send({ list: ['Sirio', 'Canopus', 'Aldebarán', 'Antares', 'Damn'] })
       .set('Accept', 'application/json')
       .expect(200, {
-        shortestLength: 4
-      })
-  })
-  it('Should get 400 with error message `List max length allowed 8`', async function () {
+        shortestLength: 4,
+      });
+  });
+
+  it('Should get 400 with error message `List max length allowed 8`', async () => {
     await this.agent
       .post('/api/shortestword')
-      .send({ list: ['Sirio', 'Canopus', 'Aldebarán', 'Antares', 'Damn', 'Altair', 'Algol', 'Aldebarán', 'Merak'] })
+      .send({
+        list: [
+          'Sirio',
+          'Canopus',
+          'Aldebarán',
+          'Antares',
+          'Damn',
+          'Altair',
+          'Algol',
+          'Aldebarán',
+          'Merak',
+        ],
+      })
       .set('Accept', 'application/json')
       .expect(400, {
-        message: 'List max length allowed 8'
-      })
-  })
-  it('Should get 400 with error message `List min length allowed 2`', async function () {
+        message: 'List max length allowed 8',
+      });
+  });
+
+  it('Should get 400 with error message `List min length allowed 2`', async () => {
     await this.agent
       .post('/api/shortestword')
       .send({ list: ['a'] })
       .set('Accept', 'application/json')
       .expect(400, {
-        message: 'List min length allowed 2'
-      })
-  })
-  it('Should get 400 with error message `List should be an Array`', async function () {
+        message: 'List min length allowed 2',
+      });
+  });
+
+  it('Should get 400 with error message `List should be an Array`', async () => {
     await this.agent
       .post('/api/shortestword')
-      .send({ list: 'Sirio, Canopus, Aldebarán, Antares, damn, Altair, Algol, Aldebarán, Merak' })
+      .send({
+        list:
+          'Sirio, Canopus, Aldebarán, Antares, damn, Altair, Algol, Aldebarán, Merak',
+      })
       .set('Accept', 'application/json')
       .expect(400, {
-        message: 'List should be an Array'
-      })
-  })
-})
+        message: 'List should be an Array',
+      });
+  });
+});
