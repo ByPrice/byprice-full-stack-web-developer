b177e4b847794f8df1900c5ac26daad1be24dcd3
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2019-03-31 16:33:31 -0600

    Fix eslint issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,24 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-    devtool: 'source-map',
-    entry: "./public-src/index.js",
-    output: {
-        path: path.join(__dirname, "/public/js"),
-        filename: "main.js"
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-react']
-                }
-            }
-        }]
-    }
-}
+  devtool: 'source-map',
+  entry: './public-src/index.js',
+  output: {
+    path: path.join(__dirname, '/public/js'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+};
