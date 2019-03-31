45bacc46f1651f4494929ded1714d2515efc0f40
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2019-03-31 12:39:55 -0600

    Resolved eslint issues in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-const rc = require('rc')
+const rc = require('rc');
 
 module.exports = rc('byprice', {
   port: process.env.PORT || 3000,
-  mongodb: process.env.MONGO_DB || 'mongodb://localhost/fullstackwebdeveloper'
-})
+  mongodb: process.env.MONGO_DB || 'mongodb://localhost/fullstackwebdeveloper',
+});
